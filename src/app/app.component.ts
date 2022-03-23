6725a8d96eda1c2622bcb0b758b1ca41b95e2005
akhanish7 <akhanish7@gmail.com>
2022-03-23 09:19:45 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,6 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent {
   name = 'Tarang';
   nameIsTarang = true;
+
+  productTitle = 'Mens yellow tshirt with cross design';
 }
